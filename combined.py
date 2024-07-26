14dfe0a0802e54813de307d338d7fe5c0757d56f
stackmuzz <61059476+stackmuzz@users.noreply.github.com>
2024-07-26 23:29:43 +0530

    Update combined.py

--- combined.py
+++ combined.py
@@ -9,29 +9,16 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
 # Constants
-SERVICE_ACCOUNT_FILE = r'C:\Users\Amrendra pratapsingh\Downloads\Email automation\service account key\mail-automation-430509-97a9a75b8baf.json'
-SPREADSHEET_ID = '1f1GxVdhHC1ezoEal3pcTitTdDvaxzHa6R90WWgl5wNg'  # Replace with your Google Sheets ID
+SERVICE_ACCOUNT_FILE = r'Path to serviceaccount.json '
+SPREADSHEET_ID = 'Sheet id '  # Replace with your Google Sheets ID
 SHEET_NAME = 'Sheet1'  # Replace with your sheet name
 EMAIL_TEMPLATE = """
 Greetings Professor,
 
-I am Amrendra Pratap Singh, a final-year B.Tech student majoring in Computer Science and Information Technology at MJP Rohilkhand University, Bareilly. I am writing to inquire about any potential internship opportunities in your research group. I am keen to apply my knowledge in ML and deep learning to practical projects under your guidance. Your research aligns closely with my academic interests, and I am eager to contribute to and learn from your esteemed team.
+Type your email here!!!!!
 
-Could we possibly schedule a brief meeting or call at your convenience to discuss any internship openings or projects where I could contribute? I would greatly appreciate your guidance on the application process or any further steps required.
-
-I have a strong passion for ML and deep learning and have been actively involved in self-study and practical applications in this field. I am eager to contribute to your ongoing projects and learn from your expertise. I am flexible regarding the internship duration and schedule. I would take care of accommodation and other related stuff on my own. Attached is my CV [here](https://drive.google.com/file/d/1ydmBYKDYyM8vvuyV9JxTH79dy7Wn-j47/view?usp=sharing) for your consideration.
-
-Thank you for considering my application. I look forward to the opportunity to work with you.
-
-Best Regards,
-
-Amrendra Pratap Singh
-
-MJP Rohilkhand University, Bareilly
-
-+91 7078045398
 """
-SUBJECT = "Inquiry regarding internship opportunity"
+SUBJECT = "Mail Subject "
 
 # Define the scopes
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly', 'https://www.googleapis.com/auth/gmail.modify']
@@ -48,7 +35,7 @@ def create_sheets_service():
 # Create the Gmail API service with OAuth 2.0
 def create_gmail_service():
     flow = InstalledAppFlow.from_client_secrets_file(
-        r'C:\Users\Amrendra pratapsingh\Downloads\Email automation\oAuth client json\client_secret_509924110112-labu54u7eq2bg0ipgtupl0vg85vb6o10.apps.googleusercontent.com.json', SCOPES)
+        r'path to client oauth.json', SCOPES)
     creds = flow.run_local_server(port=0)
     service = build('gmail', 'v1', credentials=creds)
     return service
